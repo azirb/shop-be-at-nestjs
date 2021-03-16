7131200f2cd535693cbc6c341a3108496dd4a797
rstp <cruzenepro@yandex.ru>
2021-03-16 22:58:00 +0300

    come back tp local dev

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
-  await app.listen('https://shopbepublish.herokuapp.com/');
+    await app.listen(3000);
 }
 bootstrap();
