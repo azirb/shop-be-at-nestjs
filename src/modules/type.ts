9f097b3e8d632f96e7f3c084abdd6004c5985913
rstp <cruzenepro@yandex.ru>
2021-03-16 03:44:24 +0300

    added swagger documentation to project, u can see this at /api

--- src/modules/type.ts
+++ src/modules/type.ts
@@ -2,6 +2,8 @@ import { ApiProperty } from "@nestjs/swagger";
 
 export class NotFoundResponse {
     @ApiProperty()
+    response : string; 
+    @ApiProperty()
     statusCode : number;
     @ApiProperty()
     message : string; 
