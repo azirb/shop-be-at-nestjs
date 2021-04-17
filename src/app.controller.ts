30976d364bc927e51d04472d8fd178bca15b8e4d
rstp <cruzenepro@yandex.ru>
2021-04-17 12:02:11 +0300

    delete useless thing

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller } from '@nestjs/common';
+
 
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-  @ApiTags('Hello World!')
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
-}
+export class AppController {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { Connection } from 'typeorm';
 
 @Injectable()
-export class AppService {
-  getHello():string{
-    return 'HelloWorld!';
-  }
-}
+export class AppService {}
